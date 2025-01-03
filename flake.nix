f94ecd657934f7283d05dde876bbb92189a2cfa2
Eloy Coto <eloy.coto@acalustra.com>
2025-01-03 16:06:41 +0100

    feat: update rust version
    Signed-off-by: Eloy Coto <eloy.coto@acalustra.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
@@ -24,6 +24,7 @@
             ] ++  [
               openocd
               rust-analyzer
+              picotool
             ];
           };
         }
